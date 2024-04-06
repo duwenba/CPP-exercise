ac296afb00608af03840f48bd8ed66e41cae3031
duwenba <2275317692@qq.com>
2024-04-07 00:12:58 +0800

    print every statement's value just in debug mod

--- PI/evaluator/program.cpp
+++ PI/evaluator/program.cpp
@@ -7,12 +7,17 @@
 using namespace pi::eval;
 
 shared_ptr<Object> Evaluator::evalProgram(std::list<shared_ptr<ast::Statement>> &statements) {
-    shared_ptr<Object> result = nullptr;
+    shared_ptr<Object> result;
     for(auto &statement : statements){
-        result = eval(statement);
+        result = eval((shared_ptr<pi::ast::Node> &) statement);
         if(result->getType() == Object::Type::OBJ_ERROR){
             break;
         }
+#ifdef NDEBUG
+
+#else
+        std::cout << result->toString() << std::endl;
+#endif
     }
     return result;
 }

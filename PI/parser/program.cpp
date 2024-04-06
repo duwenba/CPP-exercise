31b563d24135bf7c6c1f1cd6e1b79a0e8809acfb
duwenba <2275317692@qq.com>
2024-04-07 00:11:49 +0800

    move parserStatement function to a single file

--- PI/parser/program.cpp
+++ PI/parser/program.cpp
@@ -3,19 +3,6 @@
 using namespace pi::parser;
 using namespace pi::ast;
 
-std::shared_ptr<Statement> Parser::parseStatement() {
-    return parseExpressionStatement();
-}
-
-std::shared_ptr<ExpressionStatement> Parser::parseExpressionStatement() {
-    std::shared_ptr<ExpressionStatement> s(new ExpressionStatement());
-    s->expression = parseExpression(LOWEST);
-    s->token = currentToken;
-    while (expectPeek(token::Token::TOKEN_SEMICOLON)){
-        nextToken();
-    }
-    return s;
-}
 
 shared_ptr<Program> Parser::parseProgram() {
     std::shared_ptr<Program> s(new Program());

15d6e806ba3e560e2724fd0c7c7e2030ca55dbf3
duwenba <2275317692@qq.com>
2024-04-07 00:16:04 +0800

    强制类型转换

--- PI/evaluator/evaluator.cpp
+++ PI/evaluator/evaluator.cpp
@@ -12,26 +12,26 @@ using namespace pi::eval;
 shared_ptr<Object> Evaluator::eval(shared_ptr<Node> &node) {
     switch (node->type) {
         case Node::Type::PROGRAM:{
-            auto program = std::dynamic_pointer_cast<Program>(node);
+            auto program = std::dynamic_pointer_cast<ast::Program>(node);
             return evalProgram(program->statements);
         }
         case Node::Type::EXPRESSION_STATEMENT:{
-            auto expression = std::dynamic_pointer_cast<ExpressionStatement>(node);
-            return eval(expression->expression);
+            auto expression = std::dynamic_pointer_cast<ast::ExpressionStatement>(node);
+            return eval((shared_ptr<pi::ast::Node> &) std::move(expression->expression));
             break;
         }
         case Node::Type::INTEGER:{
-            auto integer = std::dynamic_pointer_cast<Integer>(node);
+            auto integer = std::dynamic_pointer_cast<ast::Integer>(node);
             return evalInteger(integer);
             break;
         }
         case Node::Type::INFIX:{
             auto infix = std::dynamic_pointer_cast<ast::Infix>(node);
-            auto left = eval(infix->left);
+            auto left = eval((shared_ptr<pi::ast::Node> &)infix->left);
             if (isError(left)) {
                 return left;
             }
-            auto right = eval(infix->right);
+            auto right = eval((shared_ptr<pi::ast::Node> &)infix->right);
             if (isError(right)) {
                 return right;
             }

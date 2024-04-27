a1e3a3b9a33a247731f483002a4aa34cf2cb2bdf
duwenba <2275317692@qq.com>
2024-04-27 16:46:11 +0800

    "loca"表结构定义

---
+++ TrueType/Tables/loca.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <vector>
+
+
+namespace font::table{
+	class loca {
+    protected:
+        typedef  uint32_t OffsetType;
+        typedef  vector<uint32_t> OffsetTable;
+        OffsetTable offsets;
+    public:
+        loca() = default;
+        explicit loca(uint32_t length) {
+            offsets.reserve(length);
+        }
+
+        OffsetTable * getOffsets() {
+            return &offsets;
+        }
+
+        OffsetType & offsetAt(uint16_t index){
+            return offsets.at(index);
+        }
+	};
+
+    class Sloca : public loca {
+    protected:
+        typedef  uint16_t OffsetType;
+        typedef  vector<uint16_t> OffsetTable;
+        OffsetTable offsets;
+
+    public:
+        Sloca()= default;
+        explicit Sloca(uint16_t length){
+            offsets.reserve(length);
+        }
+    };
+}
